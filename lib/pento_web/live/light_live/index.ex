efdc24093fcfcff7f0497b1b3638d7f6513a2650
Rowa <jrowah@gmail.com>
2023-07-17 18:27:59 +0300

    introduces liveview rendering

--- lib/pento_web/live/light_live/index.ex
+++ lib/pento_web/live/light_live/index.ex
@@ -1,6 +1,7 @@
 defmodule PentoWeb.LightLive.Index do
   use PentoWeb, :live_view
 
+  # Mount callback - Called when the LiveView is being mounted
   def mount(_params, _session, socket) do
     {
       :ok,
@@ -8,6 +9,7 @@ defmodule PentoWeb.LightLive.Index do
     }
   end
 
+  # Handle_event callback - Called when an event is triggered from the client
   def handle_event("on", _, socket) do
     socket = assign(socket, :brightness, 100)
     {:noreply, socket}
@@ -18,6 +20,7 @@ defmodule PentoWeb.LightLive.Index do
     {:noreply, socket}
   end
 
+  # Render callback - Called to generate the LiveView's HTML (can be inside the module or in a different .heex file)
   # def render(assigns) do
   #   ~H"""
   #   Light: <%= @brightness %>
