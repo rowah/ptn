971627d5fe8ea9fe58a5e75fe13781610429bf8a
Rowa <jrowah@gmail.com>
2023-07-19 23:41:51 +0300

    add time

--- lib/pento_web/live/wrong_live/wrong.ex
+++ lib/pento_web/live/wrong_live/wrong.ex
@@ -4,8 +4,14 @@ defmodule PentoWeb.WrongLive.Wrong do
   def mount(_params, _session, socket) do
     {
       :ok,
-      assign(socket, score: 0, message: "Guess a number")
+      socket
+      |> assign(score: 0, number_to_guess: 8, message: "Guess a number")
+      |> assign(current_time: get_current_time())
     }
+  end
+
+  defp get_current_time() do
+    DateTime.utc_now() |> DateTime.to_iso8601()
   end
 
   def handle_event("guess", %{"number" => guess} = data, socket) do
@@ -13,12 +19,14 @@ defmodule PentoWeb.WrongLive.Wrong do
     message = "Your guess is: #{guess}. Wrong guess!! Guess again."
     score = socket.assigns.score - 1
 
+    # transform the data within socket.assigns
     {
       :noreply,
       assign(
         socket,
         score: score,
-        message: message
+        message: message,
+        current_time: get_current_time()
       )
     }
   end
@@ -28,6 +36,7 @@ defmodule PentoWeb.WrongLive.Wrong do
       <h1>Your score: <%= @score %></h1>
       <h2>
         <%= @message %>
+        It's <%= @current_time %>
       </h2>
       <h2>
         <%= for n <- 1..10 do %>
